d630f53e3e92ffd64623aecb3ba9dbf7cb35f3f9
Raymond Gregory <vinedsel@gmail.com>
2019-01-13 22:56:16 -0600

    Added base code to index.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("it's alive");
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reducer from './reducers/todo';
+
+const store = createStore(
+  reducer,
+  undefined
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
