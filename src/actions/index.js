c289e9ac40916328dd4cad5126ca13772fed2b8b
Raymond Gregory <vinedsel@gmail.com>
2019-01-14 19:34:06 -0600

    Added three more action creators.

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,40 @@ export const todoTextChanged = (text) => (
         text
     }
 )
+
+// Add a todo item
+export const addTodo = () => (
+    {
+        type: 'ADD_TODO'
+    }
+)
+
+export const todoTextChanged = (text) => (
+    {
+        type: 'TODO_TEXT_CHANGED',
+        text
+    }
+)
+
+//select a todo item
+export const todoSelected = (id) =>(
+    {
+        type: 'TODO_SELECTED',
+        id
+    }
+)
+
+//edit a todo item
+export const applyTodoEdits = (id) => (
+    {
+        type: 'APPLY_TODO_EDITS',
+        id
+    }
+)
+
+export const todoEditTextChanged = (text) => (
+    {
+        type: 'TODO_EDIT_TEXT_CHANGED',
+        text
+    }
+)
